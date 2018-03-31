f86567efba4eee9b0cb54da63f7e4c04d3fc0870
Tiago Mergulhão <me@tmergulhao.com>
2018-03-31 16:05:50 -0300

    Initial Commit

---
+++ Routines/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Routines
+//
+//  Created by Tiago Mergulhão on 31/03/18.
+//  Copyright © 2018 Tiago Mergulhão. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
